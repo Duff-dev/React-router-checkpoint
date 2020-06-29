36ef7b35635a9f1b2c4ba6f779a915b923f04f9f
riadh <ftouhi.riadh@gmail.com>
2020-06-29 15:22:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
-function App() {
+import Home from "./components/Home";
+import Category from "./components/Category";
+import Products from "./components/Products";
+
+import "./App.css";
+
+const fakeAuth = {
+  isAuthenticated: true,
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/category" component={Category} />
+        <Route path="/products" component={Products} />
+        <PrivateRoute
+          authed={fakeAuth.isAuthenticated}
+          path="/products"
+          component={Products}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
